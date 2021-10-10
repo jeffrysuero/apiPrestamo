98533e425b15711dfd8a86e59418b88117683720
jeffrysuero <jeffrysuero@hotmail.com>
2021-10-10 19:37:22 -0400

    inicio proyecto

---
+++ src/main/java/com/Prestamo/App/PrestamoApplication.java
@@ -0,0 +1,13 @@
+package com.Prestamo.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Prestamo/App/PrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prestamo.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
