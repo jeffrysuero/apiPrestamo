7dc98c0f71cb39bc2094fd2ecaf689bade8c178f
jeffrysuero <jeffrysuero@hotmail.com>
2021-10-10 21:48:07 -0400

    clases cliente Listar

--- src/main/java/com/Prestamo/App/Controller/ClienteController.java
+++ src/main/java/com/Prestamo/App/Controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.Prestamo.App.Model.Cliente;
 import com.Prestamo.App.Service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,13 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(name = "/clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
     ClienteService clienteService;
 
+    @GetMapping
     public ResponseEntity<List<Cliente>> getAll(){
         List<Cliente> clientes = clienteService.getAll();
         if(clientes.isEmpty())
